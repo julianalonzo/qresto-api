5d5ca0d04b3a6faa37ccaaf4147474af7a7ac249
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-04 22:45:57 -0700

    Created makeFakeFood for mocking food data entity

---
+++ __test__/fixtures/food.js
@@ -0,0 +1,23 @@
+import faker from 'faker'
+import uuid from 'uuid/v4'
+
+const Id = Object.freeze({
+  makeId: uuid,
+  isValidId: uuid.isUuid
+})
+
+export default function makeFakeFood (overrides) {
+  const food = {
+    id: Id.makeId(),
+    name: faker.commerce.productName(),
+    description: faker.lorem.sentences(2),
+    price: faker.commerce.price(),
+    available: faker.random.boolean(),
+    restaurantId: Id.makeId()
+  }
+
+  return {
+    ...food,
+    ...overrides
+  }
+}
