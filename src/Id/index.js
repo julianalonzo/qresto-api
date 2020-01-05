661559765b0c6e3091f7298c975b388505d1d8e0
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-04 23:12:38 -0700

    Changed isValidId implementation in Id entity

--- src/Id/index.js
+++ src/Id/index.js
@@ -2,7 +2,7 @@ import uuid from 'uuid/v4'
 
 const Id = Object.freeze({
   makeId: uuid,
-  isValidId: uuid.isUuid
+  isValidId: (str) => /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89ABab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/.test(str)
 })
 
 export default Id
