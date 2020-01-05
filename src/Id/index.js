a92fbdab30da3f538fd56914ff9222ecbe13c644
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-04 23:41:14 -0700

    Formatted code in Id

--- src/Id/index.js
+++ src/Id/index.js
@@ -2,7 +2,10 @@ import uuid from 'uuid/v4'
 
 const Id = Object.freeze({
   makeId: uuid,
-  isValidId: (str) => /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89ABab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/.test(str)
+  isValidId: str =>
+    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89ABab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/.test(
+      str
+    )
 })
 
 export default Id
