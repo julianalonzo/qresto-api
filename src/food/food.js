8c4ec4417480a64e202e22fe4f8c0ffdcebf08f9
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-04 22:51:57 -0700

    Validated price in food entity

--- src/food/food.js
+++ src/food/food.js
@@ -1,7 +1,13 @@
 export default function buildFood ({ Id }) {
-  return function makeFood ({ name } = {}) {
+  return function makeFood ({ name, price } = {}) {
     if (!name) {
       throw new Error('Food must have a name')
+    }
+
+    if (isNaN(price)) {
+      throw new Error('Food must have a numerical price')
+    } else if (price < 0) {
+      throw new Error('Food cannot have a negative price')
     }
   }
 }
