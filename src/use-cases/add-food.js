e972f49e22db5b4829dfa7d53248b7b47e5f1496
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-06 16:54:26 -0700

    Added add food use case implementation

---
+++ src/use-cases/add-food.js
@@ -0,0 +1,22 @@
+import makeFood from '../food'
+
+export default function makeAddFood ({ foodsDb }) {
+  return async function addFood (foodInfo) {
+    const food = makeFood(foodInfo)
+
+    const exists = await foodsDb.findById({ id: food.getId() })
+    if (exists) {
+      return exists
+    }
+
+    return foodsDb.insert({
+      id: food.getId(),
+      name: food.getName(),
+      description: food.getDescription(),
+      price: food.getPrice(),
+      available: food.isAvailable(),
+      deleted: food.isDeleted(),
+      restaurantId: food.getRestaurantId()
+    })
+  }
+}
