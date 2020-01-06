4f8c9fbf27c0abca2fb63f05f5fa16b3312085fa
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-05 18:55:45 -0700

    Created food sequelize model

---
+++ src/data-access/models/food.js
@@ -0,0 +1,28 @@
+export default function makeFoodModel ({ sequelize, DataTypes }) {
+  return sequelize.define('food', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      unique: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    name: DataTypes.STRING,
+    description: {
+      type: DataTypes.STRING,
+      defaultValue: ''
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      defaultValue: 0
+    },
+    available: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: 0
+    },
+    deleted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: 0
+    },
+    restaurantId: DataTypes.UUID
+  })
+}
