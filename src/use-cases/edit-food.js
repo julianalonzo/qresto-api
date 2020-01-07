2bd81b8ca64a1f574361f594de2c335a82b770a5
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-07 16:49:56 -0700

    Added edit food use case implementation

---
+++ src/use-cases/edit-food.js
@@ -0,0 +1,26 @@
+import makeFood from '../food'
+
+export default function makeEditFood ({ foodsDb }) {
+  return async function editFood ({ id, ...modifiedFields } = {}) {
+    if (!id) {
+      throw new Error('Food id is required')
+    }
+
+    const existing = await foodsDb.findById({ id })
+    if (!existing) {
+      throw new RangeError('Food is not found')
+    }
+
+    const modifiedFood = makeFood({ id, ...existing, ...modifiedFields })
+
+    const updatedFood = await foodsDb.update({
+      id: modifiedFood.getId(),
+      name: modifiedFood.getName(),
+      description: modifiedFood.getDescription(),
+      price: modifiedFood.getPrice(),
+      available: modifiedFood.isAvailable()
+    })
+
+    return { ...existing, ...updatedFood }
+  }
+}
