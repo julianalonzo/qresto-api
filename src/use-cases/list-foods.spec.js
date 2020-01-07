82344a4ed8248f8311d498e5049946b15e4116d7
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-07 15:18:42 -0700

    Added test suite for list-foods

---
+++ src/use-cases/list-foods.spec.js
@@ -0,0 +1,125 @@
+import Id from '../Id'
+import makeDb from '../../__test__/fixtures/db'
+import makeFakeFood from '../../__test__/fixtures/food'
+import makeFoodsDb from '../data-access/foods-db'
+import makeGetFoods from './list-foods'
+
+describe('get foods', () => {
+  let foodsDb, getFoods
+
+  beforeAll(() => {
+    foodsDb = makeFoodsDb({ makeDb })
+    getFoods = makeGetFoods({ foodsDb })
+  })
+
+  it('returns an empty array when empty', async () => {
+    const found = await getFoods()
+    expect(found).toEqual([])
+  })
+
+  it('gets all food', async () => {
+    const inserts = await Promise.all(
+      [makeFakeFood(), makeFakeFood(), makeFakeFood()].map(insert =>
+        foodsDb.insert(insert)
+      )
+    )
+
+    const found = await getFoods()
+    expect.assertions(inserts.length)
+    inserts.forEach(insert => expect(found).toContainEqual(insert))
+  })
+
+  it('can get and filter foods by availability', async () => {
+    const inserts = await Promise.all(
+      [
+        makeFakeFood({ available: true }),
+        makeFakeFood({ available: true }),
+        makeFakeFood({ available: true }),
+        makeFakeFood({ available: true }),
+        makeFakeFood({ available: true }),
+        makeFakeFood({ available: false }),
+        makeFakeFood({ available: false }),
+        makeFakeFood({ available: false }),
+        makeFakeFood({ available: false })
+      ].map(insert => foodsDb.insert(insert))
+    )
+
+    const insertedAvailableFoods = inserts.filter(insert => insert.available)
+    const availableFoodsFound = await getFoods({ available: true })
+    insertedAvailableFoods.forEach(insert =>
+      expect(availableFoodsFound).toContainEqual(insert)
+    )
+
+    const insertedUnavailableFoods = inserts.filter(insert => !insert.available)
+    const unavailableFoodsFound = await getFoods({ available: false })
+    insertedUnavailableFoods.forEach(insert =>
+      expect(unavailableFoodsFound).toContainEqual(insert)
+    )
+  })
+
+  it('can get and filter foods by deleted', async () => {
+    const inserts = await Promise.all(
+      [
+        makeFakeFood({ deleted: true }),
+        makeFakeFood({ deleted: true }),
+        makeFakeFood({ deleted: true }),
+        makeFakeFood({ deleted: true }),
+        makeFakeFood({ deleted: true }),
+        makeFakeFood({ deleted: false }),
+        makeFakeFood({ deleted: false }),
+        makeFakeFood({ deleted: false }),
+        makeFakeFood({ deleted: false })
+      ].map(insert => foodsDb.insert(insert))
+    )
+
+    const insertedDeletedFoods = inserts.filter(insert => insert.deleted)
+    const deletedFoodsFound = await getFoods({ deleted: true })
+    insertedDeletedFoods.forEach(insert =>
+      expect(deletedFoodsFound).toContainEqual(insert)
+    )
+
+    const insertedNotDeletedFoods = inserts.filter(insert => !insert.deleted)
+    const notDeletedFoodsFound = await getFoods({ deleted: false })
+    insertedNotDeletedFoods.forEach(insert =>
+      expect(notDeletedFoodsFound).toContainEqual(insert)
+    )
+  })
+
+  it('can get and filter foods by restaurantId', async () => {
+    const firstFakeRestaurantId = Id.makeId()
+    const secondFakeRestaurantId = Id.makeId()
+    const inserts = await Promise.all(
+      [
+        makeFakeFood({ restaurantId: firstFakeRestaurantId }),
+        makeFakeFood({ restaurantId: firstFakeRestaurantId }),
+        makeFakeFood({ restaurantId: firstFakeRestaurantId }),
+        makeFakeFood({ restaurantId: firstFakeRestaurantId }),
+        makeFakeFood({ restaurantId: firstFakeRestaurantId }),
+        makeFakeFood({ restaurantId: secondFakeRestaurantId }),
+        makeFakeFood({ restaurantId: secondFakeRestaurantId }),
+        makeFakeFood({ restaurantId: secondFakeRestaurantId }),
+        makeFakeFood({ restaurantId: secondFakeRestaurantId })
+      ].map(insert => foodsDb.insert(insert))
+    )
+
+    const insertedFirstRestaurantFoods = inserts.filter(
+      insert => insert.restaurantId === firstFakeRestaurantId
+    )
+    const firstRestaurantFoodsFound = await getFoods({
+      restaurantId: firstFakeRestaurantId
+    })
+    insertedFirstRestaurantFoods.forEach(insert =>
+      expect(firstRestaurantFoodsFound).toContainEqual(insert)
+    )
+
+    const insertedSecondRestaurantFoods = inserts.filter(
+      insert => insert.restaurantId === secondFakeRestaurantId
+    )
+    const secondRestaurantFoodsFound = await getFoods({
+      restaurantId: secondFakeRestaurantId
+    })
+    insertedSecondRestaurantFoods.forEach(insert =>
+      expect(secondRestaurantFoodsFound).toContainEqual(insert)
+    )
+  })
+})
