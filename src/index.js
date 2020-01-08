795eb7b99b12889996ec011cde215162b1b51154
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-07 22:21:04 -0700

    Added food routes in server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+import bodyParser from 'body-parser'
 import express from 'express'
+import { deleteFood, getFoods, patchFood, postFood } from './controllers'
+import makeCallback from './express-callback'
 
 const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/foods', makeCallback(postFood))
+app.delete('/foods', makeCallback(deleteFood))
+app.delete('/foods/:id', makeCallback(deleteFood))
+app.patch('/foods', makeCallback(patchFood))
+app.patch('/foods/:id', makeCallback(patchFood))
+app.get('/foods', makeCallback(getFoods))
 
 const PORT = 4000
 app.listen(PORT, () => {
