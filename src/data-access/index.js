6399fb2e00e62ef31d8203bee7cc5bc33004c87e
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-07 22:19:11 -0700

    Added index in data-access

---
+++ src/data-access/index.js
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv'
+import Sequelize from 'sequelize'
+import makeFoodModel from './models/food'
+import makeFoodsDb from './foods-db'
+
+dotenv.config()
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    define: {
+      timestamps: false
+    },
+    logging: false
+  }
+)
+
+const Food = makeFoodModel({ sequelize, DataTypes: Sequelize })
+
+export async function makeDb () {
+  try {
+    await sequelize.sync()
+
+    return Object.freeze({
+      Food
+    })
+  } catch (e) {
+    console.error('Unable to connect to the database', e)
+  }
+}
+
+const foodsDb = makeFoodsDb({ makeDb })
+
+const db = Object.freeze({
+  foodsDb: foodsDb
+})
+
+export default db
+export { foodsDb }
