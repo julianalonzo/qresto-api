61fdd10538b1799e7cc4a02145e1a2140228de47
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-07 22:18:00 -0700

    Removed sequelize.authenticate() in db test

--- __test__/fixtures/db.js
+++ __test__/fixtures/db.js
@@ -14,12 +14,13 @@ const FoodModel = makeFoodModel({ sequelize, DataTypes: Sequelize })
 
 export default async function makeDb () {
   try {
+    await sequelize.authenticate()
     await sequelize.sync()
 
     return Object.freeze({
       Food: FoodModel
     })
-  } catch (dbSyncError) {
-    console.log('Unable to synchronize database', dbSyncError)
+  } catch (e) {
+    console.error('Unable to connect to the database', e)
   }
 }
