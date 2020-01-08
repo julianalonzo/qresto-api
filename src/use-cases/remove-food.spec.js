92ff896f574934a6fc283b3776ad9d0e116d10cb
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-07 19:43:35 -0700

    Added test suite for remove-food use case

---
+++ src/use-cases/remove-food.spec.js
@@ -0,0 +1,38 @@
+import makeDb from '../../__test__/fixtures/db'
+import makeFakeFood from '../../__test__/fixtures/food'
+import makeFoodsDb from '../data-access/foods-db'
+import makeRemoveFood from './remove-food'
+
+describe('remove food', () => {
+  let foodsDb, removeFood
+
+  beforeAll(() => {
+    foodsDb = makeFoodsDb({ makeDb })
+    removeFood = makeRemoveFood({ foodsDb })
+  })
+
+  it('must include id', () => {
+    const foodToDelete = makeFakeFood({ id: undefined })
+    expect(removeFood(foodToDelete)).rejects.toThrow('Food id is required')
+  })
+
+  it('handles non-existent foods', async () => {
+    const fakeFood = makeFakeFood()
+    const expected = {
+      deletedCount: 0,
+      message: 'Food not found, nothing to delete'
+    }
+    const actual = await removeFood(fakeFood)
+    expect(actual).toEqual(expected)
+  })
+
+  it('hard deletes a food', async () => {
+    const fakeFood = await foodsDb.insert(makeFakeFood())
+    const expected = {
+      deletedCount: 1,
+      message: 'Food deleted'
+    }
+    const actual = await removeFood(fakeFood)
+    expect(actual).toEqual(expected)
+  })
+})
