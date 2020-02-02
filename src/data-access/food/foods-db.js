64a50c19f931c58ace2d1adf9bc7d682b9e24d59
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-02-02 15:46:33 -0700

    Updated foods-db to add images in operations

--- src/data-access/food/foods-db.js
+++ src/data-access/food/foods-db.js
@@ -10,37 +10,51 @@ export default function makeFoodsDb ({ makeDb }) {
   })
 
   async function insert ({ id = Id.makeId(), ...foodInfo }) {
-    const db = await makeDb()
-    const result = await db.Food.create({ id, ...foodInfo })
-    return result ? result.dataValues : null
+    const { Food, Image } = await makeDb()
+    const food = await Food.create({ id, ...foodInfo }, { include: Image })
+    return food ? food.toJSON() : null
   }
 
   async function findAll ({ ...query } = {}) {
-    const db = await makeDb()
-    const result = await db.Food.findAll({ where: { ...query } })
-    return result.map(({ dataValues }) => ({
-      ...dataValues
-    }))
+    const { Food, Image } = await makeDb()
+    const result = await Food.findAll({ where: { ...query }, include: Image })
+    return result.map(item => item.toJSON())
   }
 
   async function findById ({ id }) {
-    const db = await makeDb()
-    const result = await db.Food.findByPk(id)
-    return result ? result.dataValues : null
+    const { Food, Image } = await makeDb()
+    const result = await Food.findByPk(id, { include: Image })
+    return result ? result.toJSON() : null
   }
 
-  async function update ({ id, ...foodInfo }) {
-    const db = await makeDb()
-    const [affectedRows] = await db.Food.update(
-      { ...foodInfo },
-      { where: { id: id } }
-    )
-    return affectedRows > 0 ? { id, ...foodInfo } : null
+  async function update ({ id, images, ...foodInfo }) {
+    const { Food, Image } = await makeDb()
+    const food = await Food.findByPk(id)
+
+    if (food) {
+      const updatedFood = await food.update({ ...foodInfo })
+
+      if (images) {
+        const updatedImages = await Promise.all(
+          images.map(image => Image.create(image))
+        )
+
+        await updatedFood.setImages(updatedImages)
+      }
+
+      return {
+        id,
+        ...foodInfo,
+        images
+      }
+    }
+
+    return null
   }
 
   async function remove ({ id }) {
     const db = await makeDb()
-    const affectedRows = await db.Food.destroy({ where: { id: id } })
+    const affectedRows = await db.Food.destroy({ where: { id } })
     return affectedRows
   }
 }
