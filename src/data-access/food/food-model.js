d9ab1c7e2d11147a419a8b9bd29cd8dff439fd9b
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-02-02 15:44:25 -0700

    Added name option in food model

--- src/data-access/food/food-model.js
+++ src/data-access/food/food-model.js
@@ -1,24 +1,28 @@
 export default function makeFoodModel ({ sequelize, DataTypes }) {
-  return sequelize.define('food', {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      unique: true,
-      defaultValue: DataTypes.UUIDV4
+  return sequelize.define(
+    'food',
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        unique: true,
+        defaultValue: DataTypes.UUIDV4
+      },
+      name: DataTypes.STRING,
+      description: {
+        type: DataTypes.STRING,
+        defaultValue: ''
+      },
+      price: {
+        type: DataTypes.FLOAT,
+        defaultValue: 0
+      },
+      available: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+      },
+      category: DataTypes.STRING
     },
-    name: DataTypes.STRING,
-    description: {
-      type: DataTypes.STRING,
-      defaultValue: ''
-    },
-    price: {
-      type: DataTypes.FLOAT,
-      defaultValue: 0
-    },
-    available: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true
-    },
-    category: DataTypes.STRING
-  })
+    { name: { singular: 'food', plural: 'foods' } }
+  )
 }
