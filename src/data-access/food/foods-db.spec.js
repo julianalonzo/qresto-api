0fd83c902c4222656477719a69b4ed4049a4efa7
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-02-02 15:44:47 -0700

    Updated test case for foods-db

--- src/data-access/food/foods-db.spec.js
+++ src/data-access/food/foods-db.spec.js
@@ -1,19 +1,24 @@
-import makeDb from '../../../__test__/fixtures/db'
+import makeDb, { destroyDb } from '../../../__test__/fixtures/db'
 import makeFakeFood from '../../../__test__/fixtures/food'
 import makeFoodsDb from './foods-db'
 
 describe('foods db', () => {
   let foodsDb
 
-  beforeEach(async () => {
+  beforeEach(() => {
     foodsDb = makeFoodsDb({ makeDb })
+    return foodsDb
+  })
+
+  afterEach(() => {
+    return destroyDb()
   })
 
   it('inserts a food', async () => {
-    const { images, ...food } = makeFakeFood()
+    const food = makeFakeFood()
     const result = await foodsDb.insert(food)
 
-    expect(result).toEqual(food)
+    expect(result).toMatchObject(food)
   })
 
   it('lists foods', async () => {
@@ -28,7 +33,11 @@ describe('foods db', () => {
     const found = await foodsDb.findAll()
 
     expect.assertions(inserts.length)
-    inserts.forEach(insert => expect(found).toContainEqual(insert))
+    inserts.forEach(insert => expect(found).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ id: insert.id })
+      ])
+    ))
   })
 
   it('can list available foods only', async () => {
@@ -49,33 +58,31 @@ describe('foods db', () => {
     )
 
     expect.assertions(availableFoodInserts.length)
-    availableFoodInserts.forEach(insert => expect(found).toContainEqual(insert))
+    availableFoodInserts.forEach(insert => expect(found).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ id: insert.id })
+      ])
+    ))
   })
 
   it('finds a food by id', async () => {
-    const { images, ...food } = makeFakeFood()
+    const food = makeFakeFood()
     await foodsDb.insert(food)
 
     const found = await foodsDb.findById(food)
 
-    expect(found).toEqual(food)
+    expect(found).toMatchObject(food)
   })
 
   it('updates a food', async () => {
     const food = makeFakeFood()
     await foodsDb.insert(food)
 
-    food.name = 'Hamburger'
-    food.description = 'This is a hamburger'
-    food.price = 123
-    food.available = true
+    const updatedFood = makeFakeFood({ id: food.id })
 
-    const updated = await foodsDb.update(food)
+    const updated = await foodsDb.update(updatedFood)
 
-    expect(updated.name).toBe('Hamburger')
-    expect(updated.description).toBe('This is a hamburger')
-    expect(updated.price).toBe(123)
-    expect(updated.available).toBe(true)
+    expect(updated).toMatchObject(updatedFood)
   })
 
   it('deletes a food', async () => {
